08a2f462bd391a72fc6d6d88f3db2db94abd57ff
KevJSDevelopment <Kshhn12@gmail.com>
2020-09-17 13:01:05 -0400

    setup seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.delete_all
+Song.delete_all
+Genre.delete_all
+
+5.times do 
+    artist = Artist.create(name: Faker::Name.unique.name , bio: Faker::Music.unique.album) 
+    genre = Genre.create(name: Faker::Music.unique.genre) 
+    Song.create(name: Faker::Music::GratefulDead.song, artist_id: artist.id, genre_id: genre.id)
+end
